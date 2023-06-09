30aad5118ae8fcc46203984d1e9e997a21ad3ea3
st10356506 <st10356506@vcconnect.edu.za>
2023-06-09 13:44:44 +0200

    first commit

--- test/LoginP2NGTest.java
+++ test/LoginP2NGTest.java
@@ -1,17 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
- */
 
 import static org.testng.Assert.*;
 import org.testng.annotations.Test;
 
-/**
- *
- * @author HP
- */
+//https://www.youtube.com/watch?v=2EIUHHoVfmU
+//import test class
 public class LoginP2NGTest {
-    
+    static LoginP2 login1 = new LoginP2();
+    //declare new login variable
+//https://netbeans.apache.org/kb/docs/java/junit-intro.html
     public LoginP2NGTest() {
     }
 
@@ -21,14 +17,22 @@ public class LoginP2NGTest {
 
     @Test
     public void testCheckUserName() {
+        //class to check username
+        assertTrue(LoginP2.checkUserName("kyl_"));//correct format
+        assertFalse(LoginP2.checkUserName("kyle!!!!!!!"));//incorrect format
     }
 
     @Test
     public void testCheckPasswordComplexity() {
+        //class to check password complexity
+        assertTrue(LoginP2.checkPasswordComplexity("Ch&&sec@ke99!"));//correct format
+        assertFalse(LoginP2.checkPasswordComplexity("password"));    //incorrect format 
     }
 
     @Test
     public void testRegisterUser() {
+        //class to check register user
+        assertTrue(LoginP2.registerUser());
     }
 
     @Test
